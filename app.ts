7171b1d3ef4c89e6980054f2380cebbae768a51a
SenDan07 <dany.alekrmz@gmail.com>
2023-06-26 23:46:24 -0700

    First example

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('Your code goes here...')
+function add(n1: number, n2: number) {
+  return n1 + n1;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
