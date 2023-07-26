725321d0317fc0bc2ce8f94d4b41734ada87b764
SenDan07 <dany.alekrmz@gmail.com>
2023-07-25 23:46:30 -0700

    finished lesson exercise - this keyword

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 class Department {
   name: string;
 
-  constructor(n: string){
+  constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
   }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting)
+accounting.describe();
+
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+accountingCopy.describe();
