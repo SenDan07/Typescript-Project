5ff8ebbd619042bb15a19a9ad2b98f8e534bdfbe
SenDan07 <dany.alekrmz@gmail.com>
2023-07-29 21:44:04 -0700

    Finished getters and setters lesson exercise

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private id: string;
   // name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -24,19 +24,44 @@ class Department {
 
 class ITDepartment extends Department {
   admins: string[];
-  constructor(id: string, public admins: string[]) {
+  constructor(id: string, admins: string[]) {
     super(id, 'IT');
     this.admins = admins;
   }
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport(){
+    if(this.lastReport){
+      return this.lastReport;
+    }
+    throw new Error('No report found.')
+  }
+
+  set mostRecentReport(value: string){
+    if(!value){
+      throw new Error('Please pass in a valid value.')
+    }
+    this.addReport(value)
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string): void {
+    if (name === 'Dan') {
+      return;
+    }
+    this.employees.push(name);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -44,10 +69,32 @@ class AccountingDepartment extends Department {
   }
 }
 
-const accounting = new AccountingDepartment('d1', []);
 
+const it = new ITDepartment('d1', ['Dan']);
+
+it.addEmployee('Dan');
+it.addEmployee('Max');
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.mostRecentReport = 'Year End Report'
 accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
 
-// accounting.printEmployeeInformation()
+accounting.addEmployee('Dan');
+accounting.addEmployee('Max');
+
+accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
