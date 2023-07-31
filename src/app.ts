8e3e125dbffd996b0cb856bff70489ade946e26e
SenDan07 <dany.alekrmz@gmail.com>
2023-07-31 02:05:05 -0700

    Finished interfaces lesson exercises

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,45 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
-  readonly name: string;
-
   greet(phrase: string): void;
 }
 
-class Person implements Greetable, Named {
-  name: string;
+class Person implements Greetable {
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string): void {
-    console.log(`${phrase} ${this.name}`);
+    if (this.name) {
+      console.log(`${phrase} ${this.name}`);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Dan');
+user1 = new Person();
 // user1.name = 'Alex';
 
 user1.greet('Hi there, I am');
